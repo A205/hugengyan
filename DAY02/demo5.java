8be0a7096f03422c114979f161ba0d4d14af2727
hgy940720 <464851171@qq.com>
2015-07-07 19:26:36 +0800

    作业

---
+++ DAY02/demo5.java
@@ -0,0 +1,20 @@
+package hgya;
+import java.util.Scanner;
+public class demo5 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner input=new Scanner(System.in);
+System.out.println("«Î ‰»Î––∫≈£∫");
+int nmu=input.nextInt();
+for(int i=1;i<=nmu-i;i++){{
+	System.out.println(" ");
+}
+for(int j=1; j<=i*2-1;j++){
+	System.out.println("*");
+
+}
+System.out.println();
+	}
+}
+}
+
---
+++ DAY02/demo6.java
@@ -0,0 +1,23 @@
+package hgya;
+import java.util.Scanner;
+public class demo6 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner input = new Scanner(System.in);
+System.out.println("«Î ‰»Î––∫≈£∫");
+int num = input.nextInt();
+
+for(int i=1;i<=num;i++){
+	for(int j=1;j<=i;j++){
+		System.out.println("*");
+	}
+	System.out.println();
+}
+
+	}
+
+}
